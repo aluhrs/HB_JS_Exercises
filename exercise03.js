09d313ae5a7cb4fa66c797ef15acc03232d9838a
Hackbright Student <info@hackbrightacademy.com>
2014-02-27 16:48:21 -0800

    Completed exercise 3

---
+++ exercise03.js
@@ -0,0 +1,37 @@
+//TODO: check for the type
+
+function findDupe(myArray) {
+    var emptyObject = {};
+    var dupesList = [];
+    var uniqueKeys = [];
+    var item;
+
+    for (i=0; i < myArray.length; i++) {
+        item = myArray[i];
+
+        if (emptyObject.hasOwnProperty(item)) {
+            emptyObject[item]++;
+        } else {
+            emptyObject[item] = 1;
+        }
+    }
+
+    uniqueKeys = Object.getOwnPropertyNames(emptyObject);
+
+    for (i=0; i < uniqueKeys.length; i++) {
+        var key = uniqueKeys[i];
+        var count = emptyObject[key];
+
+        if (count > 1){
+
+            dupesList.push(uniqueKeys[i]);
+        }
+    }
+
+    return dupesList;
+}
+
+
+
+var votesToGoEatCake = [47, 12, 19, 22, 26, 99, 30, 50, 324, 003, 44, 33, 346, 354, 44, 235, 45, 34, 44, 590, 09, 099, 0, 1, 3, 33, 999, 9];
+console.log(findDupe(votesToGoEatCake));
